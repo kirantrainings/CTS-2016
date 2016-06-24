2a7524286d782582214c43ec4e26375f1e1fc144
kiran paturi <kpaturi@smarttuition.com>
2016-06-24 12:39:49 +0530

    added code

--- app/vehicles/vehicleSvc.js
+++ app/vehicles/vehicleSvc.js
@@ -1,27 +1,27 @@
 angular.module('vehicles')
     .service('vehicleSvc', ['$http', '$q', function ($http, $q) {
-        var vehicles = [
-            {
-                "year": 2016,
-                "make": "Audi",
-                "model": "A5"
-        },
-            {
-                "year": 2016,
-                "make": "Benz",
-                "model": "c2000"
-        },
-            {
-                "year": 2016,
-                "make": "Nissan",
-                "model": "Terrano"
-        }
-    ];
+        var vehicles;
 
 
 
         this.getVehicles = function () {
             return vehicles;
+        };
+        this.getVehiclesFromJson = function () {
+
+            var dfd = $q.defer();
+            if (vehicles) {
+                dfd.resolve(vehicles);
+            }
+            $http.get('app/data/vehicles.json').then(function (response) {
+                    vehicles = response;
+                    dfd.resolve(response);
+                })
+                .catch(function (errorResponse) {
+                    dfd.reject("error occured");
+                });
+            return dfd.promise;
+
         };
 
 }]);
@@ -30,5 +30,14 @@ angular.module('vehicles')
 angular.module('vehicles')
     .controller('vehicleCtrl', ['$scope', 'vehicleSvc', function ($scope, vehicleSvc) {
 
-        $scope.vehicles = vehicleSvc.getVehicles();
+        //$scope.vehicles = vehicleSvc.getVehicles();
+
+        vehicleSvc.getVehiclesFromJson()
+            .then(function (response) {
+                console.log(response);
+                $scope.vehicles = response.data.vehicles;
+
+            }).catch(function (errorResponse) {
+                console.log(errorResponse);
+            });
 }]);
